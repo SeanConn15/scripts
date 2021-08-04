6ba18e749849906a1a50c7fd898fe3c67d7c846c
sean <sean@sean.com>
2021-08-04 11:54:36 -0400

    make transmission script prettier

--- transmission.sh
+++ transmission.sh
@@ -1,5 +1,5 @@
 # shows status of seeded torrents
 
 terminalwidth=$(stty size | awk '{print $2}')
-# get torrent list, replace whitespace with tabs, get columns, get spaces back, cut to terminal width
-transmission-remote -l | sed 's/\s\s\+/\t/g' | cut -f6,7,8,10 | sed 's/\t/  /g' | sed "s/\(.\{$terminalwidth\}\).*/\1/"
+# get torrent list | use awk to get nice ouput | cut to terminal size
+transmission-remote -l | awk '{printf "%5s %5s %5s ", $6, $7, $8; for (i=10; i<NF; i++) printf $i " "; if (NF >= 4) print $NF;}' | sed "s/\(.\{$terminalwidth\}\).*/\1/"
